c433e73eb1ff3252c648a837d0e5b1767142f995
ΕΤΑModder <brennosmith09@gmail.com>
2025-11-17 00:31:15 +0000

    FI(L)E THE MISSILES!

--- gen.py
+++ gen.py
@@ -4,10 +4,12 @@ import os
 with open("languages.json","r",encoding="utf-8") as f:
     data=json.load(f)
 
+os.makedirs("files",exist_ok=True)
+
 exts=set()
 for lang in data:
     for e in lang.get("extensions",[]):
         exts.add(e.lstrip("."))
 
 for e in exts:
-    open(f"file.{e}","w").close()
+    open(os.path.join("files",f"file.{e}"),"w").close()
